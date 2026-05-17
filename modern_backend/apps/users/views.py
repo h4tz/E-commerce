d7756bb05eb7b57b5899a34b27b75946c0d97d90
h4tz <blizerd.777@gmail.com>
2026-05-17 21:14:43 +0000

    feat: added permissions

--- modern_backend/apps/users/views.py
+++ modern_backend/apps/users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import RegisterSerializer
 
@@ -20,3 +21,17 @@ class RegisterView(APIView):
                 status=status.HTTP_201_CREATED
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class MeView(APIView):
+    
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        
+        return Response({
+            "id": request.user.id,
+            "email": request.user.email,
+            "username": request.user.username,
+        })
+        
